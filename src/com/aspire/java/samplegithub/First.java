1e923c38c95d7ceb7124182212e3d10ab3031af6
more-prashant <prashantpopatmore@gmail.com>
2022-07-27 13:11:15 +0530

    First push

---
+++ src/com/aspire/java/samplegithub/First.java
@@ -0,0 +1,11 @@
+package com.aspire.java.samplegithub;
+
+public class First 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("hi..this is my first push on GitHub");
+		
+	}
+
+}
